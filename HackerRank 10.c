c0a2e2196cc168f92901e30012a0188ba819ff41
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-04-05 21:55:24 +0530

    Square number pattern **

---
+++ HackerRank 10.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(j=n;j>i;j--)
+        {
+            printf("%d ",j);
+        }
+        for(j=1;j<(2*i)-1;j++)
+        {
+            printf("%d ",i);
+        }
+        for(j=i;j<=n;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+    for(i=2;i<=n;i++)
+    {
+        for(j=n;j>i;j--)
+        {
+            printf("%d ",j);
+        }
+        for(j=1;j<(2*i)-1;j++)
+        {
+            printf("%d ",i);
+        }
+        for(j=i;j<=n;j++)
+        {
+            printf("%d ",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
