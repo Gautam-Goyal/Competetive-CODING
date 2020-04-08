1cd53eea33e7f542c0700a9e1c69f16aba1bc57d
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-04-08 23:31:11 +0530

    To print a new line for every space in string(char * pointer)

---
+++ HackerRank 12.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    char *s;
+    int length,i;
+    s = malloc(1024 * sizeof(char));
+    scanf("%[^\n]", s);
+    s = realloc(s, strlen(s) + 1);
+    //Write your logic to print the tokens of the sentence here.
+    length=strlen(s);
+
+    for(i=length-1;i>=0;i--)
+    {
+        if(*(s+i)==' ')
+        *(s+i)='\n';
+
+    }
+
+    for(i=0;i<=length-1;i++)
+    {
+         printf("%c",*(s+i));
+    }
+    return 0;
+
+    }
